b9d451b34597a703a7046481922390bad32be1a4
Camila Alvarez <cam.alvarez.i@gmail.com>
2025-03-26 10:35:05 -0300

    Add create node function
    Signed-off-by: Camila Alvarez <cam.alvarez.i@gmail.com>

--- riscvlab/kci/node.py
+++ riscvlab/kci/node.py
@@ -3,8 +3,13 @@ import urllib
 import logging
 import argparse
 from kci import BASE_URI
+from config import secrets
 
 logger = logging.getLogger(__name__)
+
+def _get_auth_headers():
+    return { "Authorization": f"Bearer {secrets.get('kci_token')}" }
+
 
 def get_node(node_id):
     url = urllib.parse.urljoin(urllib.parse.urljoin(BASE_URI, "/node/"), node_id)
@@ -16,7 +21,19 @@ def get_node(node_id):
     except requests.exceptions.RequestException as e:
         logger.warning(f"Failed to retrieve node: {str(e)}")
         return None
- 
+
+
+def create_node(node_data):
+    url = urllib.parse.urljoin(BASE_URI, "/node")
+    logger.debug(url)
+    try:
+        r = requests.post(url, json=node_data, headers=_get_auth_headers())
+        r.raise_for_status()
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        logger.warning(f"Failed at creating node: {stre(e)}")
+        return None
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Used for testing node-related functions")
